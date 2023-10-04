8c742b69bb6e34ed3b5b4720596cafe83d1cdcc4
GianMarcoAlagna <marcoalagna1@gmail.com>
2023-10-03 23:10:48 -0500

    initialize redux

--- client/index.jsx
+++ client/index.jsx
@@ -1,8 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App.jsx"
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App.jsx'
+import store from './utils/store.js';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
     <App />
+  </Provider>
 );
