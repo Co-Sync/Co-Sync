5c350d5860fde3a3a521c1c62e5bb3f07674c437
ash-t-luu <ashleyluu87@gmail.com>
2023-10-06 16:24:35 -0700

    sending over userSlice.js with new action creators

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,65 @@
 const path = require('path');
+const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+// Create an array to hold the plugins
+const plugins = [
+  new MiniCssExtractPlugin(),
+  new HtmlWebpackPlugin({
+    template: 'index.html',
+  }),
+  new Dotenv(),
+];
+
+// Conditionally add plugins based on development or production mode
+if (isDevelopment) {
+  plugins.push(new webpack.HotModuleReplacementPlugin());
+  plugins.push(new ReactRefreshWebpackPlugin());
+}
 
 module.exports = {
-  entry: './client/index.jsx',
+  entry: [
+    './client/index.jsx',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/dev-server',
+  ],
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
+    publicPath: '/build/', // Add publicPath for correct asset loading
   },
+  mode: isDevelopment ? 'development' : 'production',
   module: {
     rules: [
       {
-        test: /\.jsx?/,
+        test: /\.jsx?$/,
         exclude: /(node_modules)/,
         use: {
           loader: 'babel-loader',
           options: {
             presets: [
-              '@babel/preset-env', 
-              ['@babel/preset-react', {'runtime': 'automatic'}],
+              '@babel/preset-env',
+              ['@babel/preset-react', { runtime: 'automatic' }],
             ],
             plugins: [
-              'babel-plugin-styled-components'
-            ]},
+              'babel-plugin-styled-components',
+              'react-refresh/babel',
+            ].filter(Boolean),
+          },
         },
       },
       {
         test: /skin\.css$/i,
-        use: [ MiniCssExtractPlugin.loader, 'css-loader' ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.s?css$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -46,21 +69,15 @@ module.exports = {
   },
   devServer: {
     static: {
-      publicPath: '/build',
       directory: path.resolve(__dirname, 'build'),
     },
+    hot: true, // Enable HMR
     historyApiFallback: true,
     proxy: {
       '/': 'http://localhost:3000',
     },
     port: 8080,
   },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'index.html'
-    }),
-    new Dotenv(),
-  ],
-  devtool: 'source-map'
+  plugins: plugins,
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
 };
