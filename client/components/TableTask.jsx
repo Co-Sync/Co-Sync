348df8d97c914a23abb941dcb690d8ba991634f4
GianMarcoAlagna <marcoalagna1@gmail.com>
2023-10-07 17:03:24 -0500

    Modals and Requests

--- client/components/TableTask.jsx
+++ client/components/TableTask.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import { deleteTask } from '../slices/userSlice';
-import { useDispatch } from 'react-redux';
+import React from "react";
+import { deleteTask } from "../slices/userSlice";
+import { useDispatch } from "react-redux";
 
 const TableTask = ({ task }) => {
   //instead of task, might need to pass down props to
   //access props.task.taskId / props.projects.projectId
+  
   const dispatch = useDispatch();
 
-  /*update tasks access the - projectId
+  /*update tasks
+  req.body:
+  - projectId
   - columnId
    - taskId
    - taskName
    - taskComments on req.body */
 
+  /* for delete task
+   req.params:
+   - projectId
+   - columnId
+   - taskId*/
   const handleDeleteClick = (e) => {
     /*
       e.preventDefault(); 
@@ -33,11 +41,11 @@ const TableTask = ({ task }) => {
         console.error('Delete Task ERROR: ', err);
       });
      }*/
-  }
+  };
 
   return (
-    <div className='container' id='tableTaskMain'>
-      <p>{ task.taskName }</p>
+    <div className="container" id="tableTaskMain">
+      <p>{task.taskName}</p>
       <button onClick={(e) => handleDeleteClick(e)}>Delete Task</button>
     </div>
   );
