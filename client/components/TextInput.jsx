74f5355fb5a069b490df2948f333ba1b010a25cd
GianMarcoAlagna <marcoalagna1@gmail.com>
2023-10-07 02:34:12 -0500

    Font Changes, fixed some inconsistent spacing with the font in the notched outline text fields

--- client/components/TextInput.jsx
+++ client/components/TextInput.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 const TextInput = ({ placeholder, onChange, value }) => {
+  const legendWidth = `${placeholder.length * 0.32}em`;
   return (
     <div className='TextInputOuter'>
-      <fieldset className='TextField'>
+      <fieldset className='TextField' style={{ '--legend-width': legendWidth }}>
         <input
           className='TextInput'
           type="text"
